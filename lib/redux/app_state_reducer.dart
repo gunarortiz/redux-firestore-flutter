93e16ff5d8ad03d1e974743906c6795fa9336797
Gunar Ortiz <gunarortiz@gmail.com>
2019-05-09 11:39:33 -0400

    first commit

---
+++ lib/redux/app_state_reducer.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_redux/redux/actions.dart';
+import 'package:firebase_redux/redux/app_state.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_epics/redux_epics.dart';
+import 'package:rxdart/rxdart.dart';
+
+
+AppState appStateReducer(AppState state, dynamic action) {
+  return new AppState(
+    counter: counterReducer(state.counter, action),
+  );
+}
+
+final counterReducer = combineReducers<int>([
+  new TypedReducer<int, CounterOnDataEventAction>(_setCounter),
+]);
+
+int _setCounter(int oldCounter, CounterOnDataEventAction action) {
+  return action.counter;
+}
+
---
+++ lib/redux/middleware.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_redux/redux/actions.dart';
+import 'package:firebase_redux/redux/app_state.dart';
+import 'package:redux_epics/redux_epics.dart';
+import 'package:rxdart/rxdart.dart';
